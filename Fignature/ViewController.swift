228b2868b81e702dea591abb7199b88bef1727e3
Michael Pchelnikov <pchelnikov@MacBook-Pro-Mikhail.local>
2014-08-19 18:51:26 +0400

    Initial Commit

---
+++ Fignature/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fignature
+//
+//  Created by Mikhail Pchelnikov on 19/08/14.
+//  Copyright (c) 2014 Mikhail Pchelnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
