c2d3e2ddba62823b43f1c28ea18b81c08958702a
Michael Pchelnikov <m.pchelnikov@gmail.com>
2014-08-23 14:46:15 +0400

    Creating controls programmatically.

--- Fignature/AppDelegate.swift
+++ Fignature/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
-        // Override point for customization after application launch.
+
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
--- Fignature/ViewController.swift
+++ Fignature/ViewController.swift
@@ -18,10 +18,11 @@ class ViewController: UIViewController {
     var blue: CGFloat!
     var lastClickedButton: UIView!
     
-    @IBOutlet weak var canvas: UIImageView!
-    @IBOutlet weak var blackButton: UIButton!
-    @IBOutlet weak var blueButton: UIButton!
-    @IBOutlet weak var redButton: UIButton!
+    //Controls
+    var imageCanvasView: UIImageView!
+    var blackButton: UIButton!
+    var blueButton: UIButton!
+    var redButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,15 +31,111 @@ class ViewController: UIViewController {
         green = 0.0/255.0
         blue = 0.0/255.0
         
-        self.blackButton.layer.cornerRadius = self.blackButton.frame.size.width / 2
-        self.blackButton.layer.masksToBounds = true
-        doBorderButton(self.blackButton)
+        makeLayout()
+    }
+    
+    func makeLayout() {
+        view.backgroundColor = UIColor.whiteColor()
         
-        self.blueButton.layer.cornerRadius = self.blueButton.frame.size.width / 2
-        self.blueButton.clipsToBounds = true
+        //Canvas image
+        imageCanvasView = UIImageView()
         
-        self.redButton.layer.cornerRadius = self.redButton.frame.size.width / 2
-        self.redButton.clipsToBounds = true
+        imageCanvasView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        imageCanvasView.contentMode = UIViewContentMode.ScaleToFill
+        //imageCanvasView.backgroundColor = UIColor.grayColor()
+        
+        view.addSubview(imageCanvasView)
+        
+        //Reset button
+        let buttonReset = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        
+        buttonReset.setTranslatesAutoresizingMaskIntoConstraints(false)
+        buttonReset.frame = CGRectMake(16, 20, 40, 30)
+        buttonReset.setTitle("Reset", forState: UIControlState.Normal)
+        buttonReset.addTarget(self, action: Selector("reset:"), forControlEvents: UIControlEvents.TouchUpInside)
+        buttonReset.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        
+        view.addSubview(buttonReset)
+        
+        //Save button
+        let buttonSave = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        
+        buttonSave.setTranslatesAutoresizingMaskIntoConstraints(false)
+        buttonSave.setTitle("Save", forState: UIControlState.Normal)
+        buttonSave.addTarget(self, action: Selector("save:"), forControlEvents: UIControlEvents.TouchUpInside)
+        buttonSave.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        
+        view.addSubview(buttonSave)
+        
+        //Black button
+        blackButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        
+        blackButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        blackButton.frame = CGRectMake(0, 0, 40, 40)
+        blackButton.backgroundColor = UIColor.blackColor()
+        blackButton.addTarget(self, action: Selector("colorSelected:"), forControlEvents: UIControlEvents.TouchUpInside)
+        blackButton.tag = 0
+        blackButton.layer.cornerRadius = 15
+        blackButton.layer.masksToBounds = true
+        doBorderButton(blackButton)
+        
+        view.addSubview(blackButton)
+        
+        //Blue button
+        blueButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        
+        blueButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        blueButton.frame = CGRectMake(0, 0, 40, 40)
+        blueButton.backgroundColor = UIColor.blueColor()
+        blueButton.addTarget(self, action: Selector("colorSelected:"), forControlEvents: UIControlEvents.TouchUpInside)
+        blueButton.tag = 1
+        blueButton.layer.cornerRadius = 15
+        blueButton.layer.masksToBounds = true
+        
+        view.addSubview(blueButton)
+        
+        //Red button
+        redButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        
+        redButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+        redButton.frame = CGRectMake(0, 0, 40, 40)
+        redButton.backgroundColor = UIColor.redColor()
+        redButton.addTarget(self, action: Selector("colorSelected:"), forControlEvents: UIControlEvents.TouchUpInside)
+        redButton.tag = 2
+        redButton.layer.cornerRadius = 15
+        redButton.layer.masksToBounds = true
+        
+        view.addSubview(redButton)
+        
+        //Canvas constraints
+        view.addConstraint(NSLayoutConstraint(item: imageCanvasView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: imageCanvasView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Right, multiplier: 1.0, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: imageCanvasView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: imageCanvasView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0))
+        
+        //Reset button constraints
+        view.addConstraint(NSLayoutConstraint(item: buttonReset, attribute: NSLayoutAttribute.Top, relatedBy: .Equal, toItem: topLayoutGuide, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: buttonReset, attribute: NSLayoutAttribute.Leading, relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 5))
+        
+        //Save button constraints
+        view.addConstraint(NSLayoutConstraint(item: buttonSave, attribute: NSLayoutAttribute.Top, relatedBy: .Equal, toItem: topLayoutGuide, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0))
+        view.addConstraint(NSLayoutConstraint(item: buttonSave, attribute: NSLayoutAttribute.Trailing, relatedBy: .Equal, toItem: view, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: -5))
+        
+        //Black button contraints
+        view.addConstraint(NSLayoutConstraint(item: blackButton, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 5))
+        view.addConstraint(NSLayoutConstraint(item: blackButton, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: bottomLayoutGuide, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: -10))
+        
+        //Blue button constraints
+        view.addConstraint(NSLayoutConstraint(item: blueButton, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: blackButton, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 10))
+        view.addConstraint(NSLayoutConstraint(item: blueButton, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: bottomLayoutGuide, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: -10))
+        
+        //Red button constraints
+        view.addConstraint(NSLayoutConstraint(item: redButton, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: blueButton, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 10))
+        view.addConstraint(NSLayoutConstraint(item: redButton, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: bottomLayoutGuide, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: -10))
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,12 +144,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func reset(sender: UIButton) {
-        self.canvas.image = nil
+        self.imageCanvasView.image = nil
     }
     
     @IBAction func save(sender: UIButton) {
-        UIGraphicsBeginImageContextWithOptions(self.canvas.bounds.size, false, 0.0)
-        self.canvas.image?.drawInRect(CGRectMake(0, 0, self.canvas.frame.size.width, self.canvas.frame.size.height))
+        UIGraphicsBeginImageContextWithOptions(self.imageCanvasView.bounds.size, false, 0.0)
+        self.imageCanvasView.image?.drawInRect(CGRectMake(0, 0, self.imageCanvasView.frame.size.width, self.imageCanvasView.frame.size.height))
         var saveImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         UIImageWriteToSavedPhotosAlbum(saveImage, self, Selector("image:didFinishSavingWithError:contextInfo:"), nil)
@@ -105,7 +202,7 @@ class ViewController: UIViewController {
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
         for touch: AnyObject in touches {
-            lastPoint = touch.locationInView(self.view)
+            lastPoint = touch.locationInView(view)
         }
     }
     
@@ -113,14 +210,14 @@ class ViewController: UIViewController {
         var currentLocation = CGPointMake(0.0, 0.0)
         
         for touch: AnyObject in touches {
-            currentLocation = touch.locationInView(self.view)
+            currentLocation = touch.locationInView(view)
         }
         
-        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIGraphicsBeginImageContext(view.bounds.size)
         
         var ctx :CGContextRef = UIGraphicsGetCurrentContext()
         
-        self.canvas.image?.drawInRect(CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
+        self.imageCanvasView.image?.drawInRect(CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height))
         CGContextSetLineCap(ctx, kCGLineCapRound)
         CGContextSetLineWidth(ctx, brush)
         CGContextSetRGBStrokeColor(ctx, red, green, blue, 1.0)
@@ -128,7 +225,7 @@ class ViewController: UIViewController {
         CGContextMoveToPoint(ctx, lastPoint.x, lastPoint.y)
         CGContextAddLineToPoint(ctx, currentLocation.x, currentLocation.y)
         CGContextStrokePath(ctx)
-        self.canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        self.imageCanvasView.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         lastPoint = currentLocation
@@ -138,14 +235,14 @@ class ViewController: UIViewController {
         var currentLocation = CGPointMake(0.0, 0.0)
         
         for touch: AnyObject in touches {
-            currentLocation = touch.locationInView(self.view)
+            currentLocation = touch.locationInView(view)
         }
         
-        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIGraphicsBeginImageContext(view.bounds.size)
         
         var ctx :CGContextRef = UIGraphicsGetCurrentContext()
         
-        self.canvas.image?.drawInRect(CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
+        self.imageCanvasView.image?.drawInRect(CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height))
         CGContextSetLineCap(ctx, kCGLineCapRound)
         CGContextSetLineWidth(ctx, brush)
         CGContextSetRGBStrokeColor(ctx, red, green, blue, 1.0)
@@ -153,7 +250,7 @@ class ViewController: UIViewController {
         CGContextMoveToPoint(ctx, lastPoint.x, lastPoint.y)
         CGContextAddLineToPoint(ctx, currentLocation.x, currentLocation.y)
         CGContextStrokePath(ctx)
-        self.canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        self.imageCanvasView.image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
         lastPoint = currentLocation
